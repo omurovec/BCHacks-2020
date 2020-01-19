50ded3f2c95a4210c2eb40586b625da0795f0374
Owen Murovec <owenmurovec@gmail.com>
2020-01-19 08:08:02 -0800

    added styling to tutorial

--- co2tracker/src/HowTo.js
+++ co2tracker/src/HowTo.js
@@ -1,28 +1,57 @@
 import React, { Component } from "react";
 
 function HowTo() {
-  return (
-    <React.Fragment>
-      <ol>
-        <a href="https://takeout.google.com/settings/takeout">
-          <li>
-            <p>Go to this site</p>
-          </li>
-        </a>
-        <li>
-          <p>Scroll down until you find "Location History"</p>
-        </li>
-        <img src="./Assets/LocationHistory.png"></img>
-        <li>
-          <p>Make sure there is a check mark next to Location History</p>
-        </li>
-        <li>Scroll to the bottom of the page and select "Next Step"</li>
-        <li>Fill out your desired delivery method</li>
-        <li>Locate your file through your delivery methodx</li>
-        <li>Upload file to Website.</li>
-      </ol>
-    </React.Fragment>
-  );
+	return (
+		<div className="how-to">
+			<ol>
+				<li>
+					<p>
+						Follow this{" "}
+						<a href="https://takeout.google.com/settings/takeout">
+							link
+						</a>{" "}
+						to download your data.
+					</p>
+				</li>
+				<li>
+					<p>
+						Scroll down until you find "Location History"
+						and ake sure there is a check mark next to
+						Location History
+					</p>
+				</li>
+				<img
+					src={require("./Assets/LocationHistory.png")}
+				></img>{" "}
+				<li>
+					<p>
+						Scroll to the bottom of the page and select
+						"Next Step"
+					</p>
+				</li>
+				<img src={require("./Assets/next-step.png")}></img>{" "}
+				<li>
+					<p>Fill out the form as shown below:</p>
+				</li>
+				<img
+					src={require("./Assets/export-settings.png")}
+				></img>{" "}
+				<li>
+					<p>Check your email for the download link</p>
+				</li>
+				<li>
+					<p>
+						Upload the latest year of data to the main page
+						<br />
+						<span>
+							(found in Takeout\Location History\Semantic
+							Location History\[LATEST_YEAR])
+						</span>{" "}
+					</p>
+				</li>
+			</ol>
+		</div>
+	);
 }
 
 export default HowTo;
