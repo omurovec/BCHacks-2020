6d747c4d24def546e1967113c7280761d3d0793d
LukeSparg <lukesparg@hotmail.ca>
2020-01-19 02:37:49 -0800

    Added Dropdown menus, still need to add the database connection functionality

--- co2tracker/src/Dropdown.js
+++ co2tracker/src/Dropdown.js
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function Dropdown(props) {
-	return <div></div>;
+	var years = ["2002", "2005", "3006"];
+	var make = ["Toyota", "Mazda", "Kia"];
+	var model = ["3", "5", "7"];
+    return <div>
+        <select name="Year">
+            {
+                years.map(element => {
+                    return <option value={element}>{element}</option>
+                })
+            }
+        </select>
+		<select name="Make">
+            {
+                make.map(element => {
+                    return <option value={element}>{element}</option>
+                })
+            }
+        </select>
+		<select name="Model">
+            {
+                model.map(element => {
+                    return <option value={element}>{element}</option>
+                })
+            }
+        </select>
+    </div>;
 }
